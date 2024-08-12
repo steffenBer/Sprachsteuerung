e433241860a884fda832ef587a3b790a754c56bb
steffenBer <56956218+steffenBer@users.noreply.github.com>
2024-08-13 00:01:13 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io" ) }
+    }
+}
+
+rootProject.name = "MySpeechRecognition"
+include(":app")
+ 
